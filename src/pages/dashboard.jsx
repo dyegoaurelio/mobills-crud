28b6cafe700a0c73ce7e41c4bba852fb7541848a
Dyego Aurelo <dyegoaurelio@gmail.com>
2021-02-12 20:15:31 -0300

    dashboard basic style

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import { Header } from '../components/fixed'
-import { Page, Content } from '../styles'
+import { Page, DashboardContent, CardArea } from '../styles'
+import Card from '@material-ui/core/Card'
 
 function Dashboard () {
   return (
     <Page>
       <Header />
-        <Content>
-        this is the dashboard page
-        </Content>
+        <DashboardContent>
+        <CardArea>
+          <h2>Adesão</h2>
+          <body>
+            <Card>Adicionar despesa</Card>
+            <Card>Alterar informação</Card>
+            <Card>Adicionar receita</Card>
+          </body>
+        </CardArea>
+        <CardArea>
+          <h2>Acompanhamento</h2>
+          <body>
+            <Card>Gráficos</Card>
+            <Card>saldo</Card>
+            <Card>histórico</Card>
+          </body>
+        </CardArea>
+        </DashboardContent>
     </Page>
   )
 }
--- src/styles.js
+++ src/styles.js
@@ -10,3 +10,35 @@ export const Content = styled.div`
 grid-column: 2 / 3;
 grid-row: 2 / 3;
 `
+
+export const DashboardContent = styled.div` 
+grid-area: 2 / 2 / 3 / 3;
+display: grid;
+grid-template-rows: 1fr 2fr ;
+width: 100%;
+`
+
+// eslint-disable-next-line react/react-in-jsx-scope
+export const CardArea = styled.div` 
+display: grid;
+grid-template: auto 1fr / 1fr ;
+
+h1, h2 {
+    margin-left: 5%;
+    grid-area: 1 / 1 / 2 / 2;
+}
+
+body{
+    grid-area: 2 / 1 / 3 / 2 ;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .MuiCard-root{
+        height: 50%;
+        width: 25%;
+        padding: 15px;
+        margin: 20px;
+    }
+}
+`
