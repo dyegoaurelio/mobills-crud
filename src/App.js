3cb861414d6433f1d9c5db68e78436e7815a0841
Dyego Aurelo <dyegoaurelio@gmail.com>
2021-02-13 17:36:11 -0300

    read transactions and balance working

--- src/App.js
+++ src/App.js
@@ -11,30 +11,15 @@ import {
 } from 'react-router-dom'
 
 import { Dashboard, NotFoundPage } from './pages'
+import { readTransactions } from './util/firestoreFunctions'
 
-import firebase from './util/firebase'
-const teste = async () => {
-  const db = await firebase.firestore()
-  // console.log(data.exists)
-  const docRef = db.collection('users').doc('teste')
-
-  docRef.get().then(function (doc) {
-    if (doc.exists) {
-      console.log('Document data:', doc.data())
-    } else {
-      // doc.data() will be undefined in this case
-      console.log('No such document!')
-    }
-  }).catch(function (error) {
-    console.log('Error getting document:', error)
-  })
-}
 function App () {
   const dispatch = useDispatch()
   dispatch(addUrlPath('dashboard', '/'))
   dispatch(addUrlPath('not-found-page', '/404'))
-  dispatch(setUser('usuarioTESTE'))
-  teste()
+  dispatch(setUser('teste'))
+  console.log(readTransactions(999))
+
   return (
       <Router>
         <Switch>
---
+++ src/util/firestoreFunctions.js
@@ -0,0 +1,32 @@
+import firebase from './firebase'
+import { useSelector } from 'react-redux'
+
+const db = firebase.firestore()
+
+/**
+ * will return all user's transactions since specified date
+ *
+ * if no date specified, returns all transactions
+ * @param {number?} date minimum timestamp to fetch data
+ * @returns {Array<object>}
+ */
+export async function readTransactions (date) {
+  const id = useSelector(({ userId }) => userId)
+  const docRef = db.collection('users').doc(id).collection('transactions')
+  let snapshot
+
+  if (typeof date === 'number') {
+    snapshot = await docRef.where('timestamp', '>', date).get()
+    return snapshot.docs.map(doc => doc.data())
+  }
+
+  snapshot = await docRef.get()
+  return snapshot.docs.map((doc) => doc.data())
+}
+
+export async function readBalance () {
+  const id = useSelector(({ userId }) => userId)
+  const docRef = db.collection('users').doc(id)
+
+  return await (await docRef.get()).data()
+}
